caf203db8e18a30188e53b29167054b3056e579f
Edson Costa <edson.13.10.2004@gmail.com>
2023-08-19 19:30:44 -0300

    Primas service update

--- src/urubu/urubu.service.ts
+++ src/urubu/urubu.service.ts
@@ -3,7 +3,7 @@ import {
   NotFoundException,
   BadRequestException,
 } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
 import * as schedule from 'node-schedule';
 
